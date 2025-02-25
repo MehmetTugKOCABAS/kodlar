d471408127e2724bce0929e2e83218f1d289b88e
MehmetTugKOCABAS <mehmettugkocabas4@gmail.com>
2025-02-25 20:04:26 +0300

    Add files via upload

---
+++ Ogrenci.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp23
+{
+    internal class Ogrenci
+    {
+        public int No;
+        public string Ad;
+        public string Soyad;
+        public string Sube;
+        public int sira;
+
+        public Ogrenci()
+        {
+            Console.WriteLine("bir nesen olusturuldu");
+        }
+        public int yas(int dogumyil)
+        {
+            return  DateTime.Now.Year - dogumyil;
+           
+
+        }
+    }
+}
